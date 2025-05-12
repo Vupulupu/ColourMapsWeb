99d37a592443b281c1f72c0d6e0084b370ae3c3e
Vupulupu <71806946+Vupulupu@users.noreply.github.com>
2025-05-12 02:00:29 -0600

    Send initial POST request to log into database

--- src/app/manage/manage.component.ts
+++ src/app/manage/manage.component.ts
@@ -19,6 +19,7 @@ export class ManageComponent {
   username: string = '';
   password: string = '';
   databaseName: string = '';
+  colors: Color[] = [];
 
   isNewDatabase: boolean = false;
   colorsFetched: boolean = false;
@@ -27,5 +28,30 @@ export class ManageComponent {
   private http = inject(HttpClient);
 
   signIntoDatabase(): void {
+    const requestBody: { [key: string]: number | string | boolean } = {
+      "username": this.username,
+      "password": this.password,
+      "database": this.databaseName,
+    };
+    if (this.isNewDatabase)
+      requestBody["database-action"] = true;
+
+    this.http.post<{[key: string]: {}[]}>(this.apiUrl, requestBody).subscribe({
+      next: res => {
+        console.log(res["colors"]);
+        this.colors = res["colors"] as Color[];
+        this.colorsFetched = true;
+      },
+      error: err => {
+        console.error(err.error);
+        this.authenticateError = err.error.error;
+      }
+    });
+  }
 }
+
+interface Color {
+  id: number;
+  name: string;
+  hex: string;
 }
